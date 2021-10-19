1773013247377566133e22e112e14cd305ad4d24
reader-wh94 <sstt9884@gmail.com>
2021-10-19 13:24:39 +0900

    feat: 변수 전달 과정 변경

--- 알고리즘/Algorithm_JAVA/src/Insertion.java
+++ 알고리즘/Algorithm_JAVA/src/Insertion.java
@@ -1,21 +1,21 @@
 public class Insertion {
     int i, j;
-    int []a = {7, 2, 3, 9, 1};
 
-    public void insertionSort() {
-        for(i = 1; i < a.length; i++) {
-            int standard = a[i];
+    public void insertionSort(int []data) {
+        int size = data.length;
+        for(i = 1; i < size; i++) {
+            int standard = data[i];
             j = i - 1; // j는 정렬이 끝난 끝 지점을 가리킴
 
-            while(j >= 0 && standard < a[j]) {
-                a[j+1] = a[j];
+            while(j >= 0 && standard < data[j]) {
+                data[j+1] = data[j];
                 j--;
             }
-            a[j+1] = standard;
+            data[j+1] = standard;
         }
 
-        for(i=0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
+        for(i=0; i < size; i++) {
+            System.out.print(data[i] + " ");
         }
     }
 }
