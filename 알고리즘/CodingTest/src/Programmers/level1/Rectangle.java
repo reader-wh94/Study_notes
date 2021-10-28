aad305490a2af2da4cbbd76ab9ffaae4b8475409
reader-wh94 <sstt9884@gmail.com>
2021-10-28 14:34:50 +0900

    Print Rectangluar star

--- 알고리즘/CodingTest/src/Programmers/level1/Rectangle.java
+++ 알고리즘/CodingTest/src/Programmers/level1/Rectangle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 // https://programmers.co.kr/learn/courses/30/lessons/12969
 
-public class Solution {
+public class Rectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
