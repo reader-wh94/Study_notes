8bb0177eb9329706ca31923c958a17517d44acd9
reader-wh94 <sstt9884@gmail.com>
2021-11-06 21:55:38 +0900

    Convert String to Integer

---
+++ 알고리즘/CodingTest/src/Programmers/level1/StringToInt.java
@@ -0,0 +1,12 @@
+package Programmers.level1;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+
+public class StringToInt {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
