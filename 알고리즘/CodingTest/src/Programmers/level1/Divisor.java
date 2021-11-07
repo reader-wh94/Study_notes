af3e98fe31952943c48a11bffc54dc55b1e4b5ae
reader-wh94 <sstt9884@gmail.com>
2021-11-07 14:47:37 +0900

    Find the sum of divisor

---
+++ 알고리즘/CodingTest/src/Programmers/level1/Divisor.java
@@ -0,0 +1,17 @@
+package Programmers.level1;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12928
+
+public class Divisor {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(n % i == 0) {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
