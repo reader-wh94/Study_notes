afa4485e032b42537ee8d7eb625341610fbc5b30
reader-wh94 <sstt9884@gmail.com>
2021-12-21 23:00:04 +0900

    Add annotations

--- 알고리즘/CodingTest/src/Retry/Marathon.java
+++ 알고리즘/CodingTest/src/Retry/Marathon.java
@@ -26,3 +26,9 @@ public class Marathon {
         return answer;
     }
 }
+
+/*
+*   동명이인이 있기 때문에 <String, Integer>로 해결해야한다. <Integer, String>이 아님!
+*   map.getOrDefault 함수를 통해 만약 해당 사람이 이미 해쉬맵에 있다면(동명이인) value을 +1 하거나 -1 한다.
+*   결국 key 값 전체를 검사하여 해당 키의 value 값이 0인 사람을 찾아 return 한다.
+* */
