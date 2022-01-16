e0b73827cbeaf20c8417a28c2aae80cb00ee67fb
reader-wh94 <sstt9884@gmail.com>
2022-01-16 17:55:13 +0900

    Return the number of ways to express n with successive natural numbers

---
+++ 알고리즘/CodingTest/src/Programmers/level2/NaturalNumber.java
@@ -0,0 +1,21 @@
+package Programmers.level2;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12924
+
+public class NaturalNumber {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i <= n; i++) {
+            int sum = 0;
+            for(int j = i; j <= n; j++) {
+                sum += j;
+                if(sum == n) {
+                    answer++;
+                    break;
+                } else if(sum > n) break;
+            }
+        }
+        return answer;
+    }
+}
