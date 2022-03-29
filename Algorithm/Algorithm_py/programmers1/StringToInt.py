81a894a1177e85b43c22bd819c0603d56d83212d
reader-wh94 <sstt9884@gmail.com>
2022-03-29 13:24:49 +0900

    feat: Algorithms for Strings

---
+++ Algorithm/Algorithm_py/programmers1/StringToInt.py
@@ -0,0 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12925?language=python3
+
+def solution(s):
+  answer = int(s)
+  return answer
---
+++ Algorithm/Algorithm_py/programmers1/Watermelon.py
@@ -0,0 +1,6 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12922?language=python3
+
+def solution(n):
+  answer = ''
+  
+  return "수박" * (n//2) + "수" *(n%2)
