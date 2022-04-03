06a502ad1a8f9a84f8d7163166a9bb6a7f6b9ede
reader-wh94 <sstt9884@gmail.com>
2022-04-03 13:24:18 +0900

    Sort each digit of an integer in descending order

--- Algorithm/Algorithm_py/programmers1/SortN.py
+++ Algorithm/Algorithm_py/programmers1/SortN.py
@@ -1,7 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12933
 
 def solution(n):
-  arr = list(str(n))
+  arr = list(str(int(n)))
   arr.sort(reverse=True)
   
   answer = int(''.join(arr)) 
