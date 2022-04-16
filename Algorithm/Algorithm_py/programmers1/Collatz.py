a7a540361e8b58f64a26e84e7ffa93ab477fadff
reader-wh94 <sstt9884@gmail.com>
2022-04-16 12:19:40 +0900

    Find the number of Collatz conjectures

---
+++ Algorithm/Algorithm_py/programmers1/Collatz.py
@@ -0,0 +1,16 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12943?language=python3
+def solution(num):
+  answer = 0
+  
+  while num != 1:
+    if num % 2 == 0:
+      num //= 2
+    else:
+      num = (num * 3) + 1
+    answer += 1
+    
+    if answer == 500:
+      answer = -1
+      break
+  
+  return answer
