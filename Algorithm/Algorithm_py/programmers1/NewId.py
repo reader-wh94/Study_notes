456173ae94c6b211517a0d6287369e1da129e5aa
reader-wh94 <sstt9884@gmail.com>
2022-04-20 11:12:08 +0900

    Recommend a new ID that meets the conditions

---
+++ Algorithm/Algorithm_py/programmers1/NewId.py
@@ -0,0 +1,23 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72410?language=python3
+import re
+
+def solution(new_id):
+  answer = new_id
+  answer = answer.lower()
+  answer = re.sub('[^a-z0-9\-_.]', '', answer)
+  answer = re.sub('\.+', '.', answer)
+  answer = answer.strip('.')
+  
+  if len(answer) <= 0:
+    answer += 'a'
+  
+  if len(answer) >= 16:
+    answer = answer[ :15]
+    answer = answer.rstrip('.')
+  elif len(answer) <=2:
+    while len(answer) < 3:
+      answer += answer[-1]
+   
+  return answer
+
+solution('=.=')
