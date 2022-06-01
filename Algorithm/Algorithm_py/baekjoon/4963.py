ddd99161b6523ce161747d705371c0fd42d3c5b8
reader-wh94 <sstt9884@gmail.com>
2022-06-01 12:17:33 +0900

    Solved the 4963 problem again

--- Algorithm/Algorithm_py/baekjoon/4963.py
+++ Algorithm/Algorithm_py/baekjoon/4963.py
@@ -1,36 +1,34 @@
 # https://www.acmicpc.net/problem/4963
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 
-dx=[-1,0,1,0,-1,-1,1,1]
-dy=[0,1,0,-1,-1,1,-1,1]
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
 def dfs(x, y):
+  graph[x][y] = -1
+
   for i in range(8):
     nx = x + dx[i]
     ny = y + dy[i]
-    
-    if (0 <= nx < h) and (0 <= ny < w):
+
+    if 0 <= nx < h and 0 <= ny < w:
       if graph[nx][ny] == 1:
-        graph[nx][ny] = 0
+        graph[nx][ny] = -1
         dfs(nx, ny)
 
 while True:
   w, h = map(int, input().split())
+  graph = [list(map(int, input().split())) for _ in range(h)]
+  count = 0
+
   if w == 0 and h == 0:
     break
-  
-  graph = []
-  for _ in range(h):
-    graph.append(list(map(int, input().split())))
-  
-  count = 0
-  
+
   for i in range(h):
     for j in range(w):
       if graph[i][j] == 1:
-        count += 1
-        graph[i][j] == 0
         dfs(i, j)
-  
+        count += 1
+
   print(count)
