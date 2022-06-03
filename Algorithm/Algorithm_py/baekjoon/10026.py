7adc1b2486144b007ac23ac67573bd89adcfb405
reader-wh94 <sstt9884@gmail.com>
2022-06-03 15:04:18 +0900

    Solved the 10026 problem again

--- Algorithm/Algorithm_py/baekjoon/10026.py
+++ Algorithm/Algorithm_py/baekjoon/10026.py
@@ -1,40 +1,39 @@
 # https://www.acmicpc.net/problem/10026
-import sys
-sys.setrecursionlimit(10000)
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-def dfs(x,y):
-  visited[y][x] = 1
-  
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
-    
-    if (0 <= nx < N) and (0 <= ny < N):
-      if visited[ny][nx] == 0 and graph[y][x] == graph[ny][nx]:
-        dfs(nx, ny)
-        
 N = int(input())
 graph = [list(map(str, input())) for _ in range(N)]
 visited = [[0]*N for _ in range(N)]
 count1 = 0
 
-for i in range(N):
+def dfs(x, y):
+  visited[y][x] = 1
+
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+
+    if 0 <= nx < N and 0 <= ny < N:
+      if visited[ny][nx] == 0 and graph[ny][nx] == graph[y][x]:
+        dfs(nx, ny)
+
+for i in range(N): # 적록색맹 아닌 경우
   for j in range(N):
     if visited[i][j] == 0:
       count1 += 1
       dfs(j, i)
-      
-for i in range(N):
+
+for i in range(N): # 적록색맹인 경우 R을 G로 변경
   for j in range(N):
     if graph[i][j] == 'R':
       graph[i][j] = 'G'
 
 count2 = 0
 visited = [[0]*N for _ in range(N)]
-for i in range(N):
+
+for i in range(N): # 적록색맹인 경우 dfs 재시작
   for j in range(N):
     if visited[i][j] == 0:
       count2 += 1
