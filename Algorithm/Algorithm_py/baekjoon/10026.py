74c36ce0d14dbb1872d2dc48f552e12f01f66575
reader-wh94 <sstt9884@gmail.com>
2022-06-04 15:57:53 +0900

    Solved the 10026 problem again

--- Algorithm/Algorithm_py/baekjoon/10026.py
+++ Algorithm/Algorithm_py/baekjoon/10026.py
@@ -1,12 +1,14 @@
 # https://www.acmicpc.net/problem/10026
+import sys
+sys.setrecursionlimit(100000)
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 N = int(input())
 graph = [list(map(str, input())) for _ in range(N)]
-visited = [[0]*N for _ in range(N)]
-count1 = 0
+visited = [[0] * N for _ in range(N)]
+count1, count2 = 0, 0
 
 def dfs(x, y):
   visited[y][x] = 1
@@ -25,15 +27,14 @@ for i in range(N): # 적록색맹 아닌 경우
       count1 += 1
       dfs(j, i)
 
-for i in range(N): # 적록색맹인 경우 R을 G로 변경
+for i in range(N): # 적록생맹 경우를 위해 R을 G로 변경
   for j in range(N):
     if graph[i][j] == 'R':
       graph[i][j] = 'G'
 
-count2 = 0
-visited = [[0]*N for _ in range(N)]
+visited = [[0] * N for _ in range(N)] # visited 초기화
 
-for i in range(N): # 적록색맹인 경우 dfs 재시작
+for i in range(N): #적록생맹의 경우
   for j in range(N):
     if visited[i][j] == 0:
       count2 += 1
