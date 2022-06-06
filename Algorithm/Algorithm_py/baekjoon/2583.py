1647ebbfe2db7ba7f1eeb9b19e01baffe07f3125
reader-wh94 <sstt9884@gmail.com>
2022-06-06 23:01:01 +0900

    Solved the 2583 problem again

--- Algorithm/Algorithm_py/baekjoon/2583.py
+++ Algorithm/Algorithm_py/baekjoon/2583.py
@@ -1,37 +1,39 @@
 # https://www.acmicpc.net/problem/2583
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 
-N, M, K = map(int, input().split())
-graph = [[0]*N for _ in range(M)]
-result = []
-count=0
-
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 def dfs(y, x):
     global count
     graph[y][x] = 1
     count += 1
+
     for i in range(4):
-        ny = y + dy[i]
         nx = x + dx[i]
-        if(0 <= ny < M and 0 <= nx < N and graph[ny][nx] == 0):
-            dfs(ny, nx)
+        ny = y + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[ny][nx] == 0:
+                dfs(ny, nx)
+
+M, N, K = map(int, input().split())
+graph = [[0]*N for _ in range(M)]
+result = []
+count = 0
 
 for _ in range(K):
-    y1, x1, y2, x2 = map(int, input().split())
-    for j in range(y1, y2):
-        for i in range(x1, x2):
-            graph[j][i] = -1
+    x1,y1,x2,y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] = -1
 
-
-for j in range(M):
-    for i in range(N):
-        if(graph[j][i] == 0):
-            count=0
-            dfs(j, i)
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 0:
+            count = 0
+            dfs(i, j)
             result.append(count)
 
 result.sort()
