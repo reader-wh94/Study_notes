2f3b4fc545662c13e0eb4b90ef256a11c34901a1
reader-wh94 <sstt9884@gmail.com>
2022-06-14 22:38:09 +0900

    Solved the 1743 problem with dfs again

--- Algorithm/Algorithm_py/baekjoon/1743.py
+++ Algorithm/Algorithm_py/baekjoon/1743.py
@@ -1,25 +1,25 @@
 # https://www.acmicpc.net/problem/1743
 import sys
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(1000000)
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 N, M, K = map(int, input().split())
 graph = [[0] * M for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 count = 0
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
 def dfs(x, y):
     global count
     visited[x][y] = True
+
     if graph[x][y] == 1:
         count += 1
 
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-
         if 0 <= nx < N and 0 <= ny < M:
             if graph[nx][ny] == 1 and visited[nx][ny] == False:
                 dfs(nx, ny)
@@ -35,5 +35,4 @@ for i in range(N):
             count = 0
             dfs(i, j)
             res = max(res, count)
-
 print(res)
