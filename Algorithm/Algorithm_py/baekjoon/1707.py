2a9c8f96d2e3b91355fffb097dc54a1574e534a0
reader-wh94 <sstt9884@gmail.com>
2022-06-21 21:27:24 +0900

    Solved the 1707 problem again with bfs

--- Algorithm/Algorithm_py/baekjoon/1707.py
+++ Algorithm/Algorithm_py/baekjoon/1707.py
@@ -1,40 +1,4 @@
 # https://www.acmicpc.net/problem/1707
-"""import sys
-sys.setrecursionlimit(10**6)
-
-K = int(input())
-
-def dfs(v, group):
-    visited[v] = group
-
-    for i in graph[v]:
-        if visited[i] == 0:
-            if not dfs(i, -group):
-                return False
-        elif visited[i] == visited[v]:
-                return False
-    return True
-
-for _ in range(K):
-    V, E = map(int, input().split())
-    graph = [[] for _ in range(V + 1)]
-    visited = [0] * (V + 1)
-
-    for _ in range(E):
-        u, v = map(int, input().split())
-        graph[u].append(v)
-        graph[v].append(u)
-
-    result = True
-
-    for i in range(1, V + 1):
-        if visited[i] == 0:
-            result = dfs(i, 1)
-            if not result:
-                break
-
-    print("YES" if result else "NO")"""
-#dfs는 시간초과 or 메모리 초과
 
 from collections import deque
 
@@ -46,13 +10,13 @@ def bfs(v):
     queue.append(v)
 
     while queue:
-        a = queue.popleft()
-        for i in graph[a]:
+        num = queue.popleft()
+        for i in graph[num]:
             if visited[i] == 0:
-                visited[i] = -visited[a]
+                visited[i] = -visited[num]
                 queue.append(i)
             else:
-                if visited[i] == visited[a]:
+                if visited[i] == visited[num]:
                     return False
     return True
 
@@ -60,16 +24,17 @@ for _ in range(K):
     V, E = map(int, input().split())
     result = True
     graph = [[] for _ in range(V + 1)]
-    visited = [0] * (V + 1)
+    visited = [1] + [0] * (V + 1)
 
     for _ in range(E):
         u, v = map(int, input().split())
         graph[u].append(v)
         graph[v].append(u)
 
-    for i in range(1, V + 1):
+    for i in range(1, V+1):
         if visited[i] == 0:
-            if not bfs(i):
+            if bfs(i) == False:
                 result = False
                 break
+
     print("YES" if result else "NO")
