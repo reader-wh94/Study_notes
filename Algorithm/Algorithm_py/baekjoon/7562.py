3cb5e3be65ae5a12a1dd0c0e5897c93d3f0dfd10
reader-wh94 <sstt9884@gmail.com>
2022-06-30 17:18:11 +0900

    Solved the 7562 problem with bfs

---
+++ Algorithm/Algorithm_py/baekjoon/7562.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/7562
+from collections import deque
+
+T = int(input())
+
+dx = [1, 2, 2, 1, -1, -2, -2, -1]
+dy = [2, 1, -1, -2, -2, -1, 1, 2]
+
+def bfs():
+    q = deque()
+    q.append((startX, startY))
+
+    while q:
+        x, y = q.popleft()
+
+        if x == endX and y == endY:
+                return graph[endX][endY] - 1
+
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < l and 0 <= ny < l:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
+
+for _ in range(T):
+    l = int(input())
+    startX, startY = map(int, input().split())
+    endX, endY = map(int, input().split())
+
+    graph = [[0] * l for _ in range(l)]
+
+    graph[startX][startY] = 1
+    print(bfs())
