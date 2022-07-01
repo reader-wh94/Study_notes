f9b2b12fc1569fc8e21c526035c227af85929a8f
reader-wh94 <sstt9884@gmail.com>
2022-07-01 16:20:46 +0900

    Solved the 1697 problem with bfs

---
+++ Algorithm/Algorithm_py/baekjoon/1697.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/1697
+from collections import deque
+
+N, K = map(int, input().split())
+visited = [0] * 100001
+max = 100000
+
+def bfs():
+    q = deque()
+    q.append(N)
+
+    while q:
+        x = q.popleft()
+
+        if x == K:
+            print(visited[x])
+            break
+
+        for i in (x-1, x+1, x*2):
+            if 0 <= i <= max and visited[i] == 0:
+                visited[i] = visited[x] + 1
+                q.append(i)
+
+bfs()
