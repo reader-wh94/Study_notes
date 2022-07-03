5b6e5591b426daa2c492fa0053703ed7b904b2cb
reader-wh94 <sstt9884@gmail.com>
2022-07-03 22:22:10 +0900

    Solved the 1012 problem with bfs again

--- Algorithm/Algorithm_py/baekjoon/1012_bfs.py
+++ Algorithm/Algorithm_py/baekjoon/1012_bfs.py
@@ -7,11 +7,11 @@ dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
 def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+    q = deque()
+    q.append((x, y))
 
-    while queue:
-        x, y = queue.popleft()
+    while q:
+        x, y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
@@ -19,21 +19,21 @@ def bfs(x, y):
 
             if 0 <= nx < N and 0 <= ny < M:
                 if graph[nx][ny] == 1:
-                    queue.append((nx, ny))
+                    q.append((nx, ny))
                     graph[nx][ny] = 0
-    return
+
 for _ in range(T):
     M, N, K = map(int, input().split())
     graph = [[0] * M for _ in range(N)]
-    count = 0
+    worm = 0
 
     for _ in range(K):
         x, y = map(int, input().split())
         graph[y][x] = 1
 
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == 1:
-                bfs(i, j)
-                count += 1
-    print(count)
+    for i in range(M):
+        for j in range(N):
+            if graph[j][i] == 1:
+                bfs(j, i)
+                worm += 1
+    print(worm)
