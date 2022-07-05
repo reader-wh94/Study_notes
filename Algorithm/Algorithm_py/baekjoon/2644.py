82e1e1094bc5de55cf5346ffb880bf440b12f533
reader-wh94 <sstt9884@gmail.com>
2022-07-05 17:59:32 +0900

    Solved the 2644 problem with bfs again

--- Algorithm/Algorithm_py/baekjoon/2644.py
+++ Algorithm/Algorithm_py/baekjoon/2644.py
@@ -1,32 +1,31 @@
 # https://www.acmicpc.net/problem/2644
 from collections import deque
 
-def bfs(start):
-    q = deque()
-    q.append(start)
-    visit = [0 for _ in range(N + 1)]
-    visit[start] = 1
-
-    while q:
-        d = q.popleft()
-        for i in graph[d]:
-            if visit[i] == 0:
-                visit[i] = 1
-                result[i] = result[d] + 1
-                q.append(i)
-
-N = int(input())
+n = int(input())
 a, b = map(int, input().split())
 m = int(input())
-graph = [[] for _ in range(N + 1)]
-result = [0 for _ in range(N + 1)]
 
-for i in range(m):
+graph = [[] for _ in range(n+1)]
+result = [0] * (n+1)
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+
+    while q:
+        v = q.popleft()
+
+        for n in graph[v]:
+            if result[n] == 0:
+                result[n] = result[v] + 1
+                # 가중치
+                q.append(n)
+
+for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
 bfs(a)
+
 print(result[b] if result[b] != 0 else -1)
-
-
