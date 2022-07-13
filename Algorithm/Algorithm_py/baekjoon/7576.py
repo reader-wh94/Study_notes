6875cd5eb4a02317e04ea44bfbee476221fdc1a7
reader-wh94 <sstt9884@gmail.com>
2022-07-13 13:43:41 +0900

    Solved the 7576 problem with bfs again

--- Algorithm/Algorithm_py/baekjoon/7576.py
+++ Algorithm/Algorithm_py/baekjoon/7576.py
@@ -4,11 +4,6 @@ from collections import deque
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-M, N = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-result = 0
-q = deque()
-
 def bfs():
     while q:
         x, y = q.popleft()
@@ -17,13 +12,18 @@ def bfs():
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M:
+            if 0 <= nx < n and 0 <= ny < m:
                 if graph[nx][ny] == 0:
                     graph[nx][ny] = graph[x][y] + 1
                     q.append((nx, ny))
 
-for i in range(N):
-    for j in range(M):
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = 0
+q = deque()
+
+for i in range(n):
+    for j in range(m):
         if graph[i][j] == 1:
             q.append((i, j))
 
@@ -37,4 +37,3 @@ for i in graph:
     result = max(result, max(i))
 
 print(result - 1)
-
