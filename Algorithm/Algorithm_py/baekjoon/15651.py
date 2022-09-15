b316df81dce806c67a4c2f204c597f50ef812563
reader-wh94 <sstt9884@gmail.com>
2022-09-15 22:52:45 +0900

    Solved the backtracking problem

---
+++ Algorithm/Algorithm_py/baekjoon/15651.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/15651
+
+n, m = map(int, input().split())
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(*s)
+        return
+
+    for i in range(1, n+1):
+        s.append(i)
+        dfs()
+        s.pop()
+
+dfs()
