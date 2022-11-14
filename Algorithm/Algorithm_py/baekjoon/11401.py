469877379d3f65780a29f593f70b97d5f22ffef2
reader-wh94 <sstt9884@gmail.com>
2022-11-14 17:16:41 +0900

    Solved the divde and conquer problem(too hard for me🥺)

---
+++ Algorithm/Algorithm_py/baekjoon/11401.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/11401
+
+n, k = map(int, input().split())
+P = 1000000007
+
+def factorial(num, mod):
+    result = 1
+    for i in range(2, num + 1):
+        result = result * i % P
+    return result
+
+def power(num, p, mod):
+    if p == 1:
+        return num % mod
+
+    if p % 2:
+        return ((power(num, p // 2, mod) ** 2) * num) % mod
+    else:
+        return (power(num, p // 2, mod) ** 2) % mod
+
+print(factorial(n, P) * power((factorial(k, P) * factorial(n - k, P)), P - 2, P) % P)
