6d84b96fad6c62e45200b4bc3fb88914031d8b5d
reader-wh94 <sstt9884@gmail.com>
2022-11-20 22:24:55 +0900

    Solved the binary search problem

---
+++ Algorithm/Algorithm_py/baekjoon/1654.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/6549
+import sys
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+lan = [int(input()) for _ in range(k)]
+start, end = 1, max(lan)
+
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+
+    for i in lan:
+        count += i // mid
+
+    if count >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
