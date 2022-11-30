e2355bd94b323ad7b92e182e13cc3749dc5d999f
reader-wh94 <sstt9884@gmail.com>
2022-11-30 13:51:15 +0900

    Solved the priority queue problem

---
+++ Algorithm/Algorithm_py/baekjoon/11286.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/11286
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+
+    if x == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(x), x))
