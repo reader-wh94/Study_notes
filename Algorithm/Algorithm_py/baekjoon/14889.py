c7203a35a7ff327acb33daa0dbb64886e12a3dac
reader-wh94 <sstt9884@gmail.com>
2023-02-05 20:59:54 +0900

    Solved the backtracking problem

--- Algorithm/Algorithm_py/baekjoon/14889.py
+++ Algorithm/Algorithm_py/baekjoon/14889.py
@@ -2,41 +2,31 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(idx):
+def dfs(depth, idx):
     global res
 
-    if idx == n // 2:
+    if depth == n // 2:
         s_res, l_res = 0, 0
-        for i in range(0, n):
-            if i not in st:
-                li.append(i)
-        for i in range(0, n // 2 - 1):
-            for j in range(i+1, n // 2):
-                s_res += arr[st[i]][st[j]] + arr[st[j]][st[i]]
-                l_res += arr[li[i]][li[j]] + arr[li[j]][li[i]]
+        for i in range(n):
+            for j in range(i+1, n):
+                if visited[i] and visited[j]:
+                    s_res += arr[i][j] + arr[j][i]
+                elif not visited[i] and not visited[j]:
+                    l_res += arr[i][j] + arr[j][i]
+        res = min(res, abs(s_res - l_res))
 
-        char = abs(s_res - l_res)
-        if res > char:
-            res = char
-        li.clear()
-        return
-
-    for i in range(n):
-        if i in st:
-            continue
-        if len(st) > 0 and st[len(st) - 1] > i:
-            continue
-        st.append(i)
-        dfs(idx + 1)
-        st.pop()
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = True
+            dfs(depth + 1, i + 1)
+            visited[i] = False
 
 n = int(input())
 arr = []
-st = []
-li = []
+visited = [False] * (n+1)
 for i in range(n):
     arr.append(list(map(int, input().split())))
 
 res = int(1e9)
-dfs(0)
+dfs(0, 0)
 print(res)
